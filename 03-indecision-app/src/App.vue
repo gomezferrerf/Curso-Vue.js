d184bddf64974ae97e9b496d40efb500dfc7688b
fgomez12 <fgomez@azimutenergia.co>
2024-10-08 14:41:24 -0500

    limpieza para segunda App

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,14 +1,5 @@
 <template>
-  <h1>Mi primera app</h1>
-  <hr />
-
-  <MyCounter v-bind:value="1" text="Hola Mundo" />
-  <MyCounterScript v-bind:value="2" />
-  <MyCounterScript2 v-bind:value="3" />
+  <h1>Mi Segunda App</h1>
 </template>
 
-<script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
-import MyCounterScript2 from './components/MyCounterScript2/MyCounterScript2.vue'
-</script>
+<script lang="ts" setup></script>
